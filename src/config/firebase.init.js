7fdf0f4e23cbd3e920317c271da27bb1c8a63a02
sanjayrautela <haribolsanjay2@gmail.com>
2024-07-20 22:26:23 +0530

    first commit

---
+++ src/config/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDtrPSg5ArBrKO-TgW4Fgw8fhkovtuftJY",
+  authDomain: "yoga-master-final-cd351.firebaseapp.com",
+  projectId: "yoga-master-final-cd351",
+  storageBucket: "yoga-master-final-cd351.appspot.com",
+  messagingSenderId: "312054718072",
+  appId: "1:312054718072:web:58f6456e39dd8e83b9d78d",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
